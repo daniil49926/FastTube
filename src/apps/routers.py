8ad92a9f6182eba48c5b3b74d2716629d1fb05dd
kaMata0 <daniil49926@gmail.com>
2023-01-18 20:09:53 +0300

    separated player

--- src/apps/routers.py
+++ src/apps/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from apps.auth.views import v1 as auth_v1
+from apps.player.views import v1 as player_v1
 from apps.user.views import v1 as user_v1
 from apps.video.views import v1 as video_v1
 
@@ -9,3 +10,4 @@ router = APIRouter()
 router.include_router(user_v1, prefix="/user/v1", tags=["user"])
 router.include_router(video_v1, prefix="/video/v1", tags=["video"])
 router.include_router(auth_v1, tags=["auth"])
+router.include_router(player_v1, prefix="/player/v1", tags=["player"])
