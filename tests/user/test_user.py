17beef89adeb70d75e4e879bd482ddd2426ceda5
kaMata0 <daniil49926@gmail.com>
2023-01-19 15:44:04 +0300

    add test db in tests

--- tests/user/test_user.py
+++ tests/user/test_user.py
@@ -1,8 +1,25 @@
 from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
 
-from core.application import get_app
+from app import app
+from core.db.database import get_db
 
-client = TestClient(get_app())
+TEST_DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+engine = create_async_engine(
+    TEST_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    yield db
+
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
 
 
 def test_healthchecker():
