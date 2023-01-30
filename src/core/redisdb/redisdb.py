681c5fc9f3899c93a1fbb9ec69cf30629d0b5341
kaMata0 <daniil49926@gmail.com>
2023-01-30 19:57:16 +0300

    added redis reuse

--- src/core/redisdb/redisdb.py
+++ src/core/redisdb/redisdb.py
@@ -2,9 +2,14 @@ import redis
 
 from core.settings import settings
 
+__as_redis = None
+
 
 def get_redis_conn() -> redis.StrictRedis:
 
+    global __as_redis
+    if __as_redis:
+        return __as_redis
     __as_redis = redis.StrictRedis(
         username=settings.REDIS_LOGIN,
         password=settings.REDIS_PWD,
