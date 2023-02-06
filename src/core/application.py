69bf264fe12c9b3d50688eaf4f5b21d096b5bc71
kaMata0 <daniil49926@gmail.com>
2023-02-06 15:06:43 +0300

    added cors

--- src/core/application.py
+++ src/core/application.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from apps.routers import router as apps_router
 from core.service.routers import router as core_router
@@ -10,7 +11,13 @@ def get_app():
     global _app
     if not _app:
         _app = FastAPI(title="FastTube", version="0.0.2", description="")
-
+        _app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["GET", "POST"],
+            allow_headers=["*"],
+        )
         _app.include_router(core_router)
         _app.include_router(apps_router)
 
